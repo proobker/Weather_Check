96753a673a8c774cbbb2574c3ce22772b083a6e8
proobker <89506631+proobker@users.noreply.github.com>
2025-08-28 13:30:56 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = 'b660e54c3b3b40af96493619242612'; // Replace with your WeatherAPI key
+const API_KEY = '########################'; // Replace with your WeatherAPI key
 const BASE_URL = 'https://api.weatherapi.com/v1/forecast.json';
 
 async function fetchWeather() {
